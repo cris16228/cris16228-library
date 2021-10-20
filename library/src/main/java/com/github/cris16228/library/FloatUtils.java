fb4420df00a36018a0914dcde7d2335c79f9edf6
Cri16228 <cpiva16@gmail.com>
2021-10-20 19:08:58 +0200

    Updated: - FloatUtils

--- library/src/main/java/com/github/cris16228/library/FloatUtils.java
+++ library/src/main/java/com/github/cris16228/library/FloatUtils.java
@@ -2,8 +2,10 @@ package com.github.cris16228.library;
 
 import android.content.Context;
 
+import java.math.BigDecimal;
 import java.math.RoundingMode;
 import java.text.NumberFormat;
+import java.util.Locale;
 
 public class FloatUtils {
 
@@ -38,18 +40,21 @@ public class FloatUtils {
     public String getNumberFormat(Object value, int limit, boolean round, RoundingMode roundingMode) throws LibraryException {
         if (limit <= 0)
             throw new LibraryException(getClass(), limit + "is not valid! It must be higher than 0");
-        float tempFloat;
-        NumberFormat format = NumberFormat.getInstance();
+        double tempDouble;
+        NumberFormat format = NumberFormat.getInstance(Locale.ENGLISH);
         format.setMinimumFractionDigits(limit);
         format.setMaximumFractionDigits(limit);
         if (round)
             format.setRoundingMode(roundingMode);
-        if (value instanceof Float || value instanceof Double)
-            return format.format(value);
+        if (value instanceof Float || value instanceof Double) {
+            BigDecimal bd = BigDecimal.valueOf((double) value);
+            return format.format(bd);
+        }
         if (value instanceof String)
             try {
-                tempFloat = Float.parseFloat((String) value);
-                return format.format(tempFloat);
+                tempDouble = Double.parseDouble((String) value);
+                BigDecimal bd = BigDecimal.valueOf((double) value);
+                return format.format(bd);
             } catch (NumberFormatException e) {
                 e.printStackTrace();
             }
