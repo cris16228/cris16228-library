e35ca95376595597ed2cc0e9384f7a0b610d9cea
Cri16228 <cpiva16@gmail.com>
2021-10-20 18:06:58 +0200

    Updated: - FloatUtils
    Added:
    DeviceUtils

---
+++ library/src/main/java/com/github/cris16228/library/DeviceUtils.java
@@ -0,0 +1,74 @@
+package com.github.cris16228.library;
+
+import android.app.Activity;
+import android.content.Context;
+import android.graphics.Point;
+import android.util.DisplayMetrics;
+import android.view.Display;
+import android.view.WindowManager;
+
+import java.math.RoundingMode;
+
+public class DeviceUtils {
+
+    Context context;
+    DisplayMetrics dm;
+
+    public static DeviceUtils with(Context _context) {
+        DeviceUtils deviceUtils = new DeviceUtils();
+        deviceUtils.context = _context;
+        deviceUtils.dm = new DisplayMetrics();
+        return deviceUtils;
+    }
+
+    public String getDisplaySize() throws LibraryException {
+        double x = 0, y = 0;
+        int mWidthPixels, mHeightPixels;
+        try {
+            WindowManager windowManager = ((Activity) context).getWindowManager();
+            Display display = windowManager.getDefaultDisplay();
+            DisplayMetrics displayMetrics = new DisplayMetrics();
+            display.getMetrics(displayMetrics);
+            Point realSize = new Point();
+            Display.class.getMethod("getRealSize", Point.class).invoke(display, realSize);
+            mWidthPixels = realSize.x;
+            mHeightPixels = realSize.y;
+            DisplayMetrics dm = new DisplayMetrics();
+            ((Activity) context).getWindowManager().getDefaultDisplay().getMetrics(dm);
+            x = Math.pow(mWidthPixels / dm.xdpi, 2);
+            y = Math.pow(mHeightPixels / dm.ydpi, 2);
+
+        } catch (Exception ex) {
+            ex.printStackTrace();
+        }
+        return FloatUtils.with(context).getNumberFormat((double) Math.sqrt(x + y), 2, true, RoundingMode.HALF_UP) + "\"";
+    }
+
+    public int getWidth() {
+        dm = context.getResources().getDisplayMetrics();
+        ((Activity) context).getWindowManager().getDefaultDisplay().getMetrics(dm);
+        return dm.widthPixels;
+    }
+
+    public int getHeight() {
+        dm = context.getResources().getDisplayMetrics();
+        ((Activity) context).getWindowManager().getDefaultDisplay().getMetrics(dm);
+        return dm.heightPixels;
+    }
+
+    public String getResolution() {
+        return getWidth() + "x" + getHeight();
+    }
+
+    public String getOrientation(int orientation) {
+        switch (orientation) {
+            case 0:
+                return "Undefined";
+            case 1:
+                return "Portrait";
+            case 2:
+                return "Landscape";
+        }
+        return "Undefined";
+    }
+}
--- library/src/main/java/com/github/cris16228/library/FloatUtils.java
+++ library/src/main/java/com/github/cris16228/library/FloatUtils.java
@@ -2,6 +2,7 @@ package com.github.cris16228.library;
 
 import android.content.Context;
 
+import java.math.RoundingMode;
 import java.text.NumberFormat;
 
 public class FloatUtils {
@@ -17,12 +18,42 @@ public class FloatUtils {
     public String getNumberFormat(Object value, int limit) throws LibraryException {
         if (limit <= 0)
             throw new LibraryException(getClass(), limit + "is not valid! It must be higher than 0");
-        NumberFormat format = NumberFormat.getCurrencyInstance();
+        float tempFloat;
+        NumberFormat format = NumberFormat.getInstance();
         format.setMinimumFractionDigits(limit);
         format.setMaximumFractionDigits(limit);
         if (value instanceof Float || value instanceof Double)
             return format.format(value);
-        else
-            throw new LibraryException(getClass(), value + "is not valid! It must be a \"float\" or a \"double\"");
+        if (value instanceof String)
+            try {
+                tempFloat = Float.parseFloat((String) value);
+                return format.format(tempFloat);
+            } catch (NumberFormatException e) {
+                e.printStackTrace();
+            }
+        else throw new LibraryException(getClass(), value + "is not valid! It must be a \"float\" or a \"double\"");
+        return (String) value;
+    }
+
+    public String getNumberFormat(Object value, int limit, boolean round, RoundingMode roundingMode) throws LibraryException {
+        if (limit <= 0)
+            throw new LibraryException(getClass(), limit + "is not valid! It must be higher than 0");
+        float tempFloat;
+        NumberFormat format = NumberFormat.getInstance();
+        format.setMinimumFractionDigits(limit);
+        format.setMaximumFractionDigits(limit);
+        if (round)
+            format.setRoundingMode(roundingMode);
+        if (value instanceof Float || value instanceof Double)
+            return format.format(value);
+        if (value instanceof String)
+            try {
+                tempFloat = Float.parseFloat((String) value);
+                return format.format(tempFloat);
+            } catch (NumberFormatException e) {
+                e.printStackTrace();
+            }
+        else throw new LibraryException(getClass(), value + "is not valid! It must be a \"float\" or a \"double\"");
+        return (String) value;
     }
 }
