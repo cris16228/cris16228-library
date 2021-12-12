32209f3b755e3a24b90c510f0982928daaac4516
cpiva <cpiva16@gmail.com>
2021-12-12 19:06:54 +0100

    Updated: - HttpUtils

---
+++ library/src/main/java/com/github/cris16228/library/HttpUtils.java
@@ -0,0 +1,55 @@
+package com.github.cris16228.library;
+
+import org.json.JSONException;
+import org.json.JSONObject;
+
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+import java.net.HttpURLConnection;
+import java.net.MalformedURLException;
+import java.net.URL;
+
+public class HttpUtils {
+
+
+    public static JSONObject getJSON(String urlString) throws IOException, JSONException {
+        HttpURLConnection urlConnection = null;
+        URL url = new URL(urlString);
+        urlConnection = (HttpURLConnection) url.openConnection();
+        urlConnection.setRequestMethod("GET");
+        urlConnection.setReadTimeout(10000 /* milliseconds */);
+        urlConnection.setConnectTimeout(15000 /* milliseconds */);
+        urlConnection.setDoOutput(true);
+        urlConnection.connect();
+
+        BufferedReader br = new BufferedReader(new InputStreamReader(url.openStream()));
+        StringBuilder sb = new StringBuilder();
+
+        String line;
+        while ((line = br.readLine()) != null) {
+            sb.append(line).append("\n");
+        }
+        br.close();
+
+        String jsonString = sb.toString();
+        System.out.println("JSON: " + jsonString);
+
+        return new JSONObject(jsonString);
+    }
+
+    public boolean isOnline(String site) throws MalformedURLException {
+        URL url = new URL(site);
+        try {
+            HttpURLConnection urlConnection = (HttpURLConnection) url.openConnection();
+            urlConnection.setRequestProperty("User-Agent", "Android Application: 1.0");
+            urlConnection.setRequestProperty("Connection", "close");
+            urlConnection.setConnectTimeout(1000 * 5); // mTimeout is in seconds
+            urlConnection.connect();
+            return urlConnection.getResponseCode() == 200;
+        } catch (IOException e) {
+            e.printStackTrace();
+        }
+        return false;
+    }
+}
