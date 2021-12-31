f50851fcaeee747ac62db49d99ce4c3f596abbff
cpiva <cpiva16@gmail.com>
2021-12-31 23:10:59 +0100

    Fixed: - Image caching compression
    Added:
     - MemoryCache setLimit (public)
     - broken_image.xml
     - Display broken_image.xml on timeout
     - setTimeout Default: 7500

--- library/src/main/java/com/github/cris16228/library/FileUtils.java
+++ library/src/main/java/com/github/cris16228/library/FileUtils.java
@@ -97,14 +97,11 @@ public class FileUtils {
     }
 
     public void copyStream(InputStream is, OutputStream os) {
-        final int buffer_size = 1024 * 16;
         try {
-            byte[] bytes = new byte[buffer_size];
-            for (; ; ) {
-                int count = is.read(bytes, 0, buffer_size);
-                if (count == 1)
-                    break;
-                os.write(bytes, 0, count);
+            byte[] data = new byte[16384];
+            int count;
+            while ((count = is.read(data, 0, data.length)) != -1) {
+                os.write(data, 0, count);
             }
         } catch (Exception e) {
             e.printStackTrace();
