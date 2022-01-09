4531821ff5840d9dccdad92b15bf38b74e724e1f
cpiva <cpiva16@gmail.com>
2022-01-09 01:56:31 +0100

    Added: - IntUtils

---
+++ library/src/main/java/com/github/cris16228/library/IntUtils.java
@@ -0,0 +1,32 @@
+package com.github.cris16228.library;
+
+import android.content.Context;
+
+import java.util.Locale;
+
+public class IntUtils {
+
+    Context context;
+
+    public static IntUtils with(Context _context) {
+        IntUtils intUtils = new IntUtils();
+        intUtils.context = _context;
+        return intUtils;
+    }
+
+    private String convertIntoTime(int ms) {
+        String time;
+        int x, seconds, minutes, hours;
+        x = ms / 1000;
+        seconds = x % 60;
+        x /= 60;
+        minutes = x % 60;
+        x /= 60;
+        hours = x % 24;
+        if (hours != 0)
+            time = String.format(Locale.US, "%02d", hours) + ":" + String.format(Locale.US, "%02d", minutes) + ":" + String.format(Locale.US, "%02d", seconds);
+        else
+            time = String.format(Locale.US, "%02d", minutes) + ":" + String.format(Locale.US, "%02d", seconds);
+        return time;
+    }
+}
