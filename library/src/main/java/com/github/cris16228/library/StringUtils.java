d57310174626af4cd7e92b664be35e0586843706
cpiva <cpiva16@gmail.com>
2022-07-14 21:16:50 +0200

    Updated StringUtils

--- library/src/main/java/com/github/cris16228/library/StringUtils.java
+++ library/src/main/java/com/github/cris16228/library/StringUtils.java
@@ -1,5 +1,7 @@
 package com.github.cris16228.library;
 
+import android.util.Log;
+
 import androidx.annotation.Nullable;
 
 import java.security.SecureRandom;
@@ -21,5 +23,33 @@ public class StringUtils {
 
     public static boolean isEmpty(@Nullable CharSequence str) {
         return str == null || str.length() == 0;
+    }
+
+    public static String stringToBinary(String text) {
+        StringBuilder bString = new StringBuilder();
+        String tmp = "";
+        if (text.trim().length() == 0) {
+            Log.e("stringToBinary", "The text cannot be empty");
+            return "";
+        }
+        for (int i = 0; i < text.length(); i++) {
+            tmp = Integer.toBinaryString(text.charAt(i));
+            for (int j = tmp.length(); j < 8; j++) {
+                tmp = "0" + tmp;
+            }
+            bString.append(tmp).append(" ");
+        }
+        Log.i("stringToBinary", bString.toString());
+        return bString.toString();
+    }
+
+    public static String binaryToString(String binaryCode) {
+        String[] code = binaryCode.split(" ");
+        StringBuilder word = new StringBuilder();
+        for (String s : code) {
+            word.append((char) Integer.parseInt(s, 2));
+        }
+        Log.i("stringToBinary", word.toString());
+        return word.toString();
     }
 }
