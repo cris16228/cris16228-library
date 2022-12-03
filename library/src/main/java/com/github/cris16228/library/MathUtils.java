ef6cb787b9b36f527243e7814d4d9b0dafdca2fb
cpiva <cpiva16@gmail.com>
2022-12-03 20:48:04 +0100

    Version 2.0.4.7.2
    Added:
     - MathUtils

---
+++ library/src/main/java/com/github/cris16228/library/MathUtils.java
@@ -0,0 +1,58 @@
+package com.github.cris16228.library;
+
+public class MathUtils {
+
+    public static String toCalculate;
+    public static String result;
+
+    public static String SolveMath(String input) {
+        input = toCalculate;
+        if (toCalculate.split("\\+").length == 2) {
+            String[] numbers = input.split("\\+");
+            try {
+                double d = Double.parseDouble(numbers[0]) + Double.parseDouble(numbers[1]);
+                result = cutDecimal(Double.toString(d));
+            } catch (Exception e) {
+                result = "NaN";
+            }
+        }
+        if (toCalculate.split("\\-").length == 2) {
+            String[] numbers = input.split("\\-");
+            try {
+                double d = Double.parseDouble(numbers[0]) - Double.parseDouble(numbers[1]);
+                result = cutDecimal(Double.toString(d));
+            } catch (Exception e) {
+                result = "NaN";
+            }
+        }
+        if (toCalculate.split("\\*").length == 2) {
+            String[] numbers = input.split("\\*");
+            try {
+                double d = Double.parseDouble(numbers[0]) * Double.parseDouble(numbers[1]);
+                result = cutDecimal(Double.toString(d));
+            } catch (Exception e) {
+                result = "NaN";
+            }
+        }
+        if (toCalculate.split("\\/").length == 2) {
+            String[] numbers = input.split("\\/");
+            try {
+                double d = Double.parseDouble(numbers[0]) / Double.parseDouble(numbers[1]);
+                result = cutDecimal(Double.toString(d));
+            } catch (Exception e) {
+                result = "NaN";
+            }
+        }
+        return "NaN";
+    }
+
+    public static String cutDecimal(String input) {
+        String[] number = input.split("\\.");
+        if (number.length > 1) {
+            if (number[1].equals("0")) {
+                input = number[0];
+            }
+        }
+        return input;
+    }
+}
