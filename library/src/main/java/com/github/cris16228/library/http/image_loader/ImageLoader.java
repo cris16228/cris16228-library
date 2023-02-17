448ae7a4c8375aa73b3db3967f6a6235ac16bee1
cpiva <cpiva16@gmail.com>
2023-02-17 18:42:58 +0100

    Version 2.0.5.3.3
    Fixed:
     - Displatcher for queueing downloads

--- library/src/main/java/com/github/cris16228/library/http/image_loader/ImageLoader.java
+++ library/src/main/java/com/github/cris16228/library/http/image_loader/ImageLoader.java
@@ -45,25 +45,6 @@ public class ImageLoader {
     private Context context;
     private boolean asBitmap = false;
 
-   /* public static ImageLoader with(Context _context, String path) {
-        ImageLoader imageLoader = new ImageLoader();
-        imageLoader.fileCache = new FileCache(path);
-        imageLoader.executor = Executors.newFixedThreadPool(3);
-        imageLoader.fileUtils = new FileUtils();
-        imageLoader.context = _context;
-        imageLoader.init();
-        return imageLoader;
-    }
-
-    public static ImageLoader with(Context _context) {
-        ImageLoader imageLoader = new ImageLoader();
-        imageLoader.fileCache = new FileCache(_context);
-        imageLoader.executor = Executors.newFixedThreadPool(3);
-        imageLoader.fileUtils = new FileUtils();
-        imageLoader.context = _context;
-        return imageLoader;
-    }*/
-
     public static ImageLoader get() {
         return new ImageLoader();
     }
@@ -336,7 +317,9 @@ public class ImageLoader {
                         loadImage.onSuccess(bitmap);
                     photoToLoad.imageView.setImageBitmap(bitmap);
                     photoToLoad.imageView.invalidate();
-                    load(urls, photoToLoad.imageView, loadImage, connectionErrors);
+                    if (urls.size() > 0) {
+                        load(urls, photoToLoad.imageView, loadImage, connectionErrors);
+                    }
                 } else {
                     if (loadImage != null)
                         loadImage.onFail();
